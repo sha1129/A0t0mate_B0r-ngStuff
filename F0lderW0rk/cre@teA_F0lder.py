8b3b2a3a54771c19e9f9d7a41558f64b11b53169
sha1129 <63379238+sha1129@users.noreply.github.com>
2022-12-25 22:15:37 -0500

    Update cre@teA_F0lder.py

--- F0lderW0rk/cre@teA_F0lder.py
+++ F0lderW0rk/cre@teA_F0lder.py
@@ -1,1 +1,21 @@
+# Create folder
+import os
 
+# Set the base path and the number of folders to create
+# r to read raw and folder path = where you want to create
+base_path = r'folder path' 
+
+# number of folder that needed to be created
+num_folders = 3
+
+# Use a for loop to create the folders
+for i in range(num_folders):
+    # Set the folder name and path
+    folder_name = 'Lecture ' + str(i+1)
+    folder_path = os.path.join(base_path, folder_name)
+    
+    # Create the folder
+    # mkdir() function is used to create the folders
+    os.mkdir(folder_path)
+
+print('Folders created!')
